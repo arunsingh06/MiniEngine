ace842357dd84ee624c04249c63eb192aa7b0356
arunsingh06 <arundeep06sh@gmail.com>
2026-07-07 18:22:14 +0200

    Add window

--- src/engine.c
+++ src/engine.c
@@ -3,12 +3,20 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+static SDL_Window *window;
 
 bool Engine_Init(void){
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
         return false;
     }
+
+    if((window = SDL_CreateWindow("Window", 0, 0, 500, 500, 0)) == NULL){
+        fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        Engine_Shutdown();
+        return false;
+    }
+
     return true;
 }
 
@@ -17,5 +25,8 @@ void Engine_Run(void){
 }
 
 void Engine_Shutdown(void){
+    if(window != NULL){
+        SDL_DestroyWindow(window);
+    }
     SDL_Quit();
 }
